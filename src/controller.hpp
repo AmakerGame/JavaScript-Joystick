cebc4371ffe5dfcde01373540e142b10334a039e
Edytor-Studio Core <volosovecadrian@gmail.com>
2026-04-14 08:57:40 +0300

    Update controller.hpp

--- src/controller.hpp
+++ src/controller.hpp
@@ -1,1 +1,19 @@
+#pragma once
+#include <vector>
+#include <cstdint>
+#include "hidapi.h"
 
+enum class ConnType { USB, BT };
+
+class PSController {
+public:
+    PSController();
+    ~PSController();
+    bool connect();
+    void update(int r, int g, int b, uint8_t motor_heavy, uint8_t motor_light);
+    
+private:
+    hid_device* handle = nullptr;
+    ConnType connection = ConnType::USB;
+    uint32_t calculate_crc32(uint8_t* data, size_t len);
+};
